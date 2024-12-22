ec2ea938a6de5a84b64769ed73138735a494fbe7
Hengkubpom <hengkubpom@gmail.com>
2024-12-22 14:18:16 +0700

    Add: CenterBox

--- src/Components/homepage/homepage.tsx
+++ src/Components/homepage/homepage.tsx
@@ -5,8 +5,12 @@ function Homepage(){
     return (
         <>
             <div>
-                <video src="/src/video/LegendNoodleGamePlay.mp4" className="main-video" autoPlay muted></video>
-
+                <video src="/src/video/LegendNoodleGamePlay.mp4" className="main-video" autoPlay muted loop></video>
+                <div className="CenterBox">
+                    <h2>LegendNoodle</h2>
+                    <p>Get ready to cook up a storm and show off your amazing culinary skills. Let's crush those sales targets!</p>
+                    <button className="playbutton">Cook Now <img src="/src/img/icon/noodle.svg" alt="noodle" /></button>
+                </div>
             </div>
         </>
     );
