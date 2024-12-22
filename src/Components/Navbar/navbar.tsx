e042fc78af6927196f9be8cb410f9ea2f136f439
Hengkubpom <hengkubpom@gmail.com>
2024-12-22 13:33:29 +0700

    Edit: navbar

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -9,14 +9,14 @@ function Navbar(){
             <img className="logo" src="/src/img/logo.png" alt="logo" />
             <div className="block-menu">
                 
-                <button className="btn">Game</button>
-                <button className="btn">Guide</button>
-                <button className="btn">Team</button>
+                <button className="btn">Game Info</button>
+                <button className="btn">Tutorial</button>
+                <button className="btn">Devloper Team <img src="/src/img/icon/dropdown.svg" alt="dropdown" /></button>
             </div>
             </div>
             <div className="right-menu">
                 
-            <button className="download">Download</button>
+            <button className="download">Download <img src="/src/img/icon/download.svg" alt="download" /></button>
             </div>
         </div>
         <Outlet />
